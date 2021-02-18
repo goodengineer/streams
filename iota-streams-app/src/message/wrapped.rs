f33839a58865f2e9e4c20d8123fcc9ae663c4904
P.M <60963077+goodengineer@users.noreply.github.com>
2021-02-18 17:27:46 +0200

    Update wrapped.rs

--- iota-streams-app/src/message/wrapped.rs
+++ iota-streams-app/src/message/wrapped.rs
@@ -18,7 +18,7 @@ pub struct WrapState<F, Link> {
 }
 
 impl<F: PRP, Link: HasLink> WrapState<F, Link> {
-    /// Save link for the current wrapped message and accociated info into the store.
+    /// Save link for the current wrapped message and associated info into the store.
     pub fn commit<Store>(
         mut self,
         mut store: RefMut<Store>,
@@ -51,7 +51,7 @@ pub struct WrappedMessage<F, Link: HasLink> {
 
 // impl<F: PRP, Link: HasLink> WrappedMessage<F, Link>
 // {
-// Save link for the current wrapped message and accociated info into the store.
+// Save link for the current wrapped message and associated info into the store.
 // pub fn commit<Store>(
 // mut self,
 // mut store: RefMut<Store>,
